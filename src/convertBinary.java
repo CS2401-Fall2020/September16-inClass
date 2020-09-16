4fd1618fbed005642603e284dfbccbffb10852f4
Dan DeBlasio <dan@dandeblasio.com>
2020-09-16 09:02:43 -0600

    Pre-class

---
+++ src/convertBinary.java
@@ -0,0 +1,23 @@
+
+public class convertBinary {
+
+  public static void main(String[] args) {
+    
+
+  }
+  
+  // Given an binary number as a string returns 
+  //  the value in base 10 as an integer using recursion. 
+  // If the string contains a character that is invalid the method returns 
+  //  -9999
+  public int binaryStringToInt(String numString) {
+    return 0;
+  }
+  
+  // Given a number in base 10, 
+  //   return a string containing the binary representation.
+  public String intToBinaryString(int numValue) {
+    return "";
+  }
+
+}
